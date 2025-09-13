1843f2137d40baba6ac71a56224c59c16d026dc3
shaziasulthana-commits <shazia.sulthana@gmail.com>
2025-09-13 12:16:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -137,7 +137,7 @@ st.markdown(
     """
     <h3 style="display: flex; align-items: center; gap: 10px;">
         <img src="https://raw.github.com/shaziasulthana-commits/BookQuizApp/main/images/EQLogo.jpg" alt="Logo" width="80">
-        EduQuest: AI Smart Book Quiz Generator
+        EduQuest: AI Smart Quiz Generator
     </h3>
     """,
     unsafe_allow_html=True
